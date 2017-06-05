2ab60ccb90fd69d606a26aad93a307e1cc372661
juasda96 <juasda96@gmail.com>
2017-06-05 12:02:15 -0500

    Final commit

--- app/js/services.js
+++ app/js/services.js
@@ -2,7 +2,8 @@
 
 var customerServices = angular.module('myApp.services', ['ngResource']);
 customerServices.factory('Customers', function ($resource) {
-    return $resource('http://localhost:8080/ClienteBackend/webresources/com.edu.udea.clientebackend.entity.cliente',
+    return $resource(
+            'http://localhost:8080/ClienteBackend/webresources/com.edu.udea.clientebackend.entity.cliente',
             {}, {query: {method: 'GET', isArray: true}
     });
 });
